a12e65dfca5bf95330aad64e2124e9e4f528fd03
five76 <fiv_e76@mail.ru>
2024-09-11 15:09:40 +0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# http://www.sphinx-doc.org/en/master/config
 
 # -- Path setup --------------------------------------------------------------
 
@@ -10,19 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
-#sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'oapisip'
-copyright = '2021, qwe'
+project = 'Сетевое и системное администрирование'
+copyright = '2020-2022'
 author = 'qwe'
-
-# The full version, including alpha/beta/rc tags
-release = '1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,8 +27,11 @@ release = '1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax'
-        ]
+extensions = [
+    "sphinx.ext.imgconverter",
+    "sphinx_inline_tabs",
+    "sphinx_copybutton",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +46,7 @@ language = 'ru'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -54,11 +54,62 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-
-html_theme = 'default'
+html_theme = 'furo'
+#html_sidebars = {
+#    "**": ["about.html", "searchbox.html", "localtoc.html", "navigation.html"]
+#}
+html_title = 'Сетевое и системное администрирование'
+html_theme_options = {
+    "prev_next_buttons_location": "both",
+    "navigation_with_keys": True,
+    "top_of_page_button": None,
+    "announcement": (
+        'Книга не обновляется! Используется версия Ansible 2.9. '
+        '<a href="https://www.youtube.com/playlist?list=PLah0HUih_ZRnuI_K5-GV4FdAO9dVkRIGF">По некоторым темам есть видео лекции.</a>'
+    ),
+    "light_css_variables": {
+        "color-brand-primary": "#336790",  # "blue"
+        "color-brand-content": "#336790",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#E5B62F",  # "yellow"
+        "color-brand-content": "#E5B62F",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ---------------------------------------------
+
+
+latex_engine = 'xelatex'
+latex_elements = {
+    'fontpkg': r'''
+\documentclass[a4paper]{article}
+\usepackage[12pt]{extsizes}
+\usepackage[margin=1.0in]{geometry}
+\usepackage{polyglossia}
+\setcounter{secnumdepth}{0}
+\setdefaultlanguage{russian}
+\setotherlanguage{english}
+\usepackage{fontspec}
+\defaultfontfeatures{Scale=MatchLowercase}
+\setmainfont{DejaVu Sans}
+\setsansfont{DejaVu Serif}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\setcounter{tocdepth}{2}
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+    'figure_align': 'H',
+}
